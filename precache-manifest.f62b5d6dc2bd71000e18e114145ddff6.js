3f4f74298b22775c67d66b88b6d5a93543ac8e53
oorzc <oorzc@qq.com>
2023-05-06 17:23:07 +0800

    Updates

---
+++ precache-manifest.f62b5d6dc2bd71000e18e114145ddff6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f09a11b2ec8384055d4331becc0ddd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e2c5f7f6523af060b133",
+    "url": "./static/css/main.4f6869ef.chunk.css"
+  },
+  {
+    "revision": "aa4ebaeff8bda09f67c1",
+    "url": "./static/js/2.d7f1d2ce.chunk.js"
+  },
+  {
+    "revision": "4cdd2411cb4fa1c1c4ef19dc4dc1d081",
+    "url": "./static/js/2.d7f1d2ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e2c5f7f6523af060b133",
+    "url": "./static/js/main.3fac6d3b.chunk.js"
+  },
+  {
+    "revision": "13af473c8fcd5a0a6f82",
+    "url": "./static/js/runtime-main.0aeefcb0.js"
+  },
+  {
+    "revision": "aab060a4b44d6b68e229dd5c63b0d016",
+    "url": "./static/media/albird.aab060a4.svg"
+  },
+  {
+    "revision": "622bb7a19e3c21858bded50c80ed5f50",
+    "url": "./static/media/bg.622bb7a1.jpeg"
+  }
+]);
